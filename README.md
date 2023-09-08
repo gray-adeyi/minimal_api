17b36d87b69b231d6c72df214615e365ff33cd86
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-09-08 11:57:54 +0100

    Add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mimimal REST API
--- main.py
+++ main.py
@@ -5,23 +5,25 @@ from datetime import datetime
 
 app = FastAPI()
 
-class Track(str, Enum):
-    BACKEND='backend'
-    FRONTEND='frontend'
-    MOBILE='mobile'
-    GENERAL='general'
-    PRODUCT_DESIGN='product_design'
-    VIDEO_MARKETING='video_marketing'
 
-@app.get('/')
-def index(slack_name: str = 'Gbenga Adeyi', track: Track = Track.BACKEND):
+class Track(str, Enum):
+    BACKEND = "backend"
+    FRONTEND = "frontend"
+    MOBILE = "mobile"
+    GENERAL = "general"
+    PRODUCT_DESIGN = "product_design"
+    VIDEO_MARKETING = "video_marketing"
+
+
+@app.get("/")
+def index(slack_name: str = "Gbenga Adeyi", track: Track = Track.BACKEND):
     timestamp = datetime.now()
     return {
-            "slack_name": slack_name,
-            "current_day": timestamp.strftime("%A"),
-            "utc_time": timestamp.strftime("%Y-%m-%dT%H:%M:%SZ"),
-            "track": track,
-            "github_file_url": "",
-            "github_repo_url": "",
-            "status_code": status.HTTP_200_OK
-            }
+        "slack_name": slack_name,
+        "current_day": timestamp.strftime("%A"),
+        "utc_time": timestamp.strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "track": track,
+        "github_file_url": "",
+        "github_repo_url": "",
+        "status_code": status.HTTP_200_OK,
+    }
